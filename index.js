d712011635946b0ac52e8530cd21516be15b8b30
Avenger7x <avenger7x13@gmail.com>
2015-02-12 14:42:03 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+	init: function(){
+		this.state = typeof this.getDefaultData === 'function' ? this.getDefaultData() : {};
+	},
+	setState: function(newState){
+		this.state = newState;
+		this.trigger(this.state);
+	},
+	getData: function(){
+		return this.state;
+	}
+};
